0c525ed50044de4b457001a7ae873cbc28bde58a
khususgaming <anggaputra.ramadian@yahoo.co.id>
2023-08-21 09:30:17 +0700

    Add project files.

---
+++ src/Domain/Sampah.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain
+{
+    public enum Sex
+    {
+        Male,
+        Female
+    }
+
+    public abstract class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Sex Sex { get; set; }
+        public virtual string Address { get; set; }
+        public virtual string Phone { get; set; }
+    }
+
+    public enum JenisSampah
+    {
+        Kertas,
+        Plastik,
+        Beling,
+        Besi
+    }
+
+    public class Sampah
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public JenisSampah JenisSampah { get; set; }
+    }
+
+
+    public class Nasabah : Person
+    {
+    }
+
+    public class Pengepul : Person
+    {
+    }
+
+    public class Petugas : Person
+    {
+        [NotMapped]
+        public override string Address { get => base.Address; set => base.Address = value; }
+        [NotMapped]
+        public override string Phone { get => base.Phone; set => base.Phone = value; }
+    }
+}
